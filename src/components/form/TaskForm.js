22659dd3873e07c1f128ece40363b5b334620b73
James <james.hinton@live.co.uk>
2022-07-29 09:26:03 +0100

    add default value

--- src/components/form/TaskForm.js
+++ src/components/form/TaskForm.js
@@ -45,6 +45,13 @@ const TaskForm = ({ task, settings, availableProjects }) => {
       },
     });
 
+  // if not hasTaskID then set the value to all the args
+  if (hasTaskID && fetchedTask) {
+    for (const arg of task.args) {
+      arg.default = fetchedTask.args[arg.name];
+    }
+  }
+
   const errors = use("errors");
 
   const createTaskField = (arg) => {
